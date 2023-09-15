9c3dbc88f21225f82aa5721c3fec0c6f60585bbe
Javed Bagwan <javed460@gmail.com>
2023-09-15 07:53:04 +0530

    Initial Project Structure

---
+++ src/main/java/com/example/instancio/SpringBootInstancioApplication.java
@@ -0,0 +1,13 @@
+package com.example.instancio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInstancioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInstancioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instancio/IntancioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instancio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntancioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
